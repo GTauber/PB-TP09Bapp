14a945be61a88e74f1b688b3d9b70fec397be816
Juan Rostirola <juanvitorrostirola666@gmail.com>
2022-09-25 17:44:40 -0300

    contacts

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <h1>Contatos</h1>
+  <div class="container-sm">
+    <ul class="list-group">
+      <li class="list-group-item" aria-current="true">Nossos Contatos</li>
+      <li class="list-group-item"><a href="https://www.linkedin.com/in/gabriel-tauber-353002202/">Gabriel Tauber</a></li>
+      <li class="list-group-item"><a href="https://www.linkedin.com/in/jo%C3%A3o-pedro-silva-caglianone-77559852/">João Caglianone</a></li>
+      <li class="list-group-item"><a href="https://www.linkedin.com/in/juan-rostirola-246695222/">Juan Rostirola</a></li>
+    </ul>
   </div>
 </template>
 
@@ -11,5 +16,16 @@ export default {
 </script>
 
 <style scoped>
-
+ul {
+  width: 20%;
+}
+div {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+li:first-child {
+  background: var(--cor-terc);
+}
 </style>
