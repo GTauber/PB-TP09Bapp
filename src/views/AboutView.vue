325ff69ed24853288a3e3f992af03e5b6af748b0
jpCaglianone <joao.caglianone@gmail.com>
2022-09-25 17:46:37 -0300

    about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <div class="card" >
+  <div class="card-body">
+    <h1 class="card-title">Quem somos</h1>
+    <p class="card-text">Amamos pelos, somos apaixonados por homens barbudos.
+       We love bears. Fazemos oque fazemos pois adoramos oque fazemos. Além do mais, sempre 
+        oferecemos uma cerveja e ótimas piadas, tudo pela satisfação do cliente, TUDO MESMO! 
+      Venha conhecer essa experiência única! </p>
+  </div>
+</div>
+
   </div>
 </template>
+
+<script></script>
+
+<style scoped>
+  p{
+    font-family:'Monotype Corsiva','Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size:40px;
+    color:var(--cor-sec);
+  }
+  .card{
+    background:rgb(223, 210, 175);
+    box-shadow: 5px 5px  20px black;
+    width: 50rem; 
+    margin: 225px auto -200px;
+  }
+  h1{
+    color: var(--cor-prim);
+  }
+</style>
+
+
